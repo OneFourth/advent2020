40aea91d8d6c16e84565613e30fbee9dcf84d4f9
Jacques Schutte <jqschutte@gmail.com>
2020-12-03 07:01:23 +0200

    Day 3 prep

--- src/lib/days/day03.rs
+++ src/lib/days/day03.rs
@@ -1,1 +1,23 @@
+use crate::lib::advent::Result;
+use crate::Day;
 
+#[derive(Default)]
+pub struct Day03;
+
+impl Day for Day03 {
+    fn number(&self) -> u8 {
+        3
+    }
+
+    fn setup(&mut self, input: &str) -> Result<()> {
+        Ok(())
+    }
+
+    fn part1(&self) -> Result<String> {
+        Ok("".to_string())
+    }
+
+    fn part2(&self) -> Result<String> {
+        Ok("".to_string())
+    }
+}
