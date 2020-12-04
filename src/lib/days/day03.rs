305f0f4990860c7622c35f9cced37c10d3f430c0
Jacques Schutte <jqschutte@gmail.com>
2020-12-04 06:52:46 +0200

    Day 3 minor cleanup

--- src/lib/days/day03.rs
+++ src/lib/days/day03.rs
@@ -10,7 +10,7 @@ struct Map {
 
 #[derive(Default)]
 pub struct Day03 {
-    map: Map
+    map: Map,
 }
 
 impl Map {
@@ -32,10 +32,8 @@ impl Map {
 
         let mut trees = 0;
         while y < self.height {
-            if let Some(c) = self.data[y].chars().nth(x) {
-                if c == '#' {
-                    trees += 1;
-                }
+            if let Some('#') = self.data[y].chars().nth(x) {
+                trees += 1;
             }
 
             x = (x + right) % self.width;
@@ -69,7 +67,34 @@ impl Day for Day03 {
             self.map.count_trees_with_slope(7, 1),
             self.map.count_trees_with_slope(1, 2),
         ];
-        let trees = slopes.iter().fold(1, |acc, t| acc * t);
+        let trees: usize = slopes.iter().product();
         Ok(trees.to_string())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Day03;
+    use crate::lib::advent::Day;
+
+    #[test]
+    fn example() {
+        let s = "\
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#";
+
+        let mut d = Day03::default();
+        d.setup(s).unwrap();
+        assert_eq!("7", d.part1().unwrap());
+        assert_eq!("336", d.part2().unwrap());
     }
 }
