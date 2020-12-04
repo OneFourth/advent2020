3e898ba17340f3e5c0828076b5f521eb182e8b8e
Jacques Schutte <jqschutte@gmail.com>
2020-12-04 09:42:52 +0200

    Day 4 part 2 fixed

--- src/lib/days/day04.rs
+++ src/lib/days/day04.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use regex::Regex;
 
 use crate::lib::advent::Result;
@@ -7,129 +5,89 @@ use crate::Day;
 
 #[derive(Debug, Default)]
 struct Passport {
-    fields: HashMap<String, String>,
+    byr: Option<String>,
+    iyr: Option<String>,
+    eyr: Option<String>,
+    hgt: Option<String>,
+    hcl: Option<String>,
+    ecl: Option<String>,
+    pid: Option<String>,
+    cid: Option<String>,
 }
 
 impl Passport {
     fn valid_part1(&self) -> bool {
-        let keys: [String; 7] = [
-            "byr".to_string(),
-            "iyr".to_string(),
-            "eyr".to_string(),
-            "hgt".to_string(),
-            "hcl".to_string(),
-            "ecl".to_string(),
-            "pid".to_string(),
-        ];
+        self.byr.is_some() &&
+        self.iyr.is_some() &&
+        self.eyr.is_some() &&
+        self.hgt.is_some() &&
+        self.hcl.is_some() &&
+        self.ecl.is_some() &&
+        self.pid.is_some()
+    }
 
-        keys.iter()
-            .filter(|k| self.fields.contains_key(k.to_owned()))
-            .count()
-            == keys.len()
+    fn valid_number_range(field: &Option<String>, min: usize, max: usize) -> bool {
+        if let Some(s) = field {
+            let num = s.parse().unwrap();
+            (min..=max).contains(&num)
+        }
+        else {
+            false
+        }
+    }
+
+    fn valid_pattern(field: &Option<String>, r: &str) -> bool {
+        if let Some(f) = field {
+            let r = Regex::new(r).unwrap();
+            r.is_match(&f)
+        }
+        else {
+            false
+        }
     }
 
     fn valid_part2(&self) -> bool {
-        {
-            let k_byr = "byr".to_string();
-            if let Some(byr) = self.fields.get(&k_byr) {
-                let byr: usize = byr.parse().unwrap();
-                if byr < 1920 || byr > 2002 {
-                    println!("{} byr", byr);
-                    return false;
-                }
-            } else {
-                return false;
-            }
+        if !Passport::valid_number_range(&self.byr, 1920, 2002) {
+            return false;
         }
 
-        {
-            let k_iyr = "iyr".to_string();
-            if let Some(iyr) = self.fields.get(&k_iyr) {
-                let iyr: usize = iyr.parse().unwrap();
-                if iyr < 2010 || iyr > 2020 {
-                    println!("{} iyr", iyr);
-                    return false;
-                }
-            } else {
-                return false;
-            }
+        if !Passport::valid_number_range(&self.iyr, 2010, 2020) {
+            return false;
         }
 
-        {
-            let k_eyr = "eyr".to_string();
-            if let Some(eyr) = self.fields.get(&k_eyr) {
-                let eyr: usize = eyr.parse().unwrap();
-                if eyr < 2020 || eyr > 2030 {
-                    println!("{} eyr", eyr);
-                    return false;
-                }
-            } else {
-                return false;
-            }
+        if !Passport::valid_number_range(&self.eyr, 2020, 2030) {
+            return false;
         }
 
-        {
-            let k_hgt = "hgt".to_string();
-            if let Some(hgt) = self.fields.get(&k_hgt) {
-                match hgt {
-                    cm if hgt.ends_with("cm") => {
-                        let cm: usize = cm[0..cm.len() - 2].parse().unwrap();
-                        if cm < 150 || cm > 193 {
-                            println!("{} cm", cm);
-                            return false;
-                        }
-                    }
-                    inches if hgt.ends_with("in") => {
-                        let inches: usize = inches[0..inches.len() - 2].parse().unwrap();
-                        if inches < 59 || inches > 76 {
-                            println!("{} inches", inches);
-                            return false;
-                        }
-                    }
-                    _ => return false,
-                }
-            } else {
-                return false;
-            }
-        }
-
-        {
-            let k_hcl = "hcl".to_string();
-            if let Some(hcl) = self.fields.get(&k_hcl) {
-                let r = Regex::new("#[0-9a-f]{6}").unwrap();
-                if !r.is_match(hcl) {
-                    println!("{} hcl", hcl);
+        if let Some(hgt) = &self.hgt {
+            let v: String = hgt[0..hgt.len() - 2].to_string();
+            if hgt.ends_with("cm") {
+                if !Passport::valid_number_range(&Some(v), 150, 193) {
                     return false;
                 }
-            } else {
+            }
+            else if hgt.ends_with("in") {
+                if !Passport::valid_number_range(&Some(v), 59, 76) {
+                    return false
+                }
+            }
+            else {
                 return false;
             }
+        } else {
+            return false;
         }
 
-        {
-            let k_ecl = "ecl".to_string();
-            if let Some(ecl) = self.fields.get(&k_ecl) {
-                let valid = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
-                if !valid.contains(&ecl.as_str()) {
-                    println!("{} ecl", ecl);
-                    return false;
-                }
-            } else {
-                return false;
-            }
+        if !Passport::valid_pattern(&self.hcl, r"^(#[0-9a-f]{6})$") {
+            return false;
         }
 
-        {
-            let k_pid = "pid".to_string();
-            if let Some(pid) = self.fields.get(&k_pid) {
-                let r = Regex::new(r"\d{9}").unwrap();
-                if !r.is_match(pid) {
-                    println!("{} pid", pid);
-                    return false;
-                }
-            } else {
-                return false;
-            }
+        if !Passport::valid_pattern(&self.ecl, r"^(amb|blu|brn|gry|grn|hzl|oth)$") {
+            return false;
+        }
+
+        if !Passport::valid_pattern(&self.pid, r"^\d{9}$") {
+            return false;
         }
 
         true
@@ -152,7 +110,18 @@ impl Day for Day04 {
 
             for field in entry.split_whitespace() {
                 let kv: Vec<_> = field.split(':').collect();
-                p.fields.insert(kv[0].to_string(), kv[1].to_string());
+                let value = Some(kv[1].to_string());
+                match kv[0] {
+                    "byr" => p.byr = value,
+                    "iyr" => p.iyr = value,
+                    "eyr" => p.eyr = value,
+                    "hgt" => p.hgt = value,
+                    "hcl" => p.hcl = value,
+                    "ecl" => p.ecl = value,
+                    "pid" => p.pid = value,
+                    "cid" => p.cid = value,
+                    _ => return Err("Invalid key".into()),
+                }
             }
 
             self.passports.push(p);
