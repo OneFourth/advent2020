824e2d23a2e597dbaba098946755830912fc4f5b
Jacques Schutte <jqschutte@gmail.com>
2020-12-05 10:13:43 +0200

    Day 5 extra cleanup

--- src/lib/days/day05.rs
+++ src/lib/days/day05.rs
@@ -4,73 +4,61 @@ use crate::lib::advent::Result;
 use crate::Day;
 
 struct Seat<'a> {
-    row_ins: &'a [u8],
-    col_ins: &'a [u8],
+    ins: &'a [u8; 10],
 }
 
 #[derive(Default)]
-pub struct Day05<'a> {
-    seats: Vec<Seat<'a>>,
+pub struct Day05 {
+    seat_ids: Vec<usize>,
 }
 
 impl<'a> Seat<'a> {
-    fn new(s: &'a [u8; 10]) -> Self {
-        let (row_ins, col_ins) = s.split_at(7);
-
-        Self { row_ins, col_ins }
-    }
-
-    fn get_value(ins: &[u8], one: u8) -> usize {
-        const BASE: usize = 2;
-        ins.iter()
-            .rev()
-            .enumerate()
-            .map(|(n, &r)| BASE.pow(n as u32) * (r == one) as usize)
-            .sum()
+    fn new(ins: &'a [u8; 10]) -> Self {
+        Self { ins }
     }
 
     fn get_seat_id(&self) -> usize {
-        let r = Seat::get_value(self.row_ins, b'B');
-        let c = Seat::get_value(self.col_ins, b'R');
-        r * 8 + c
+        const BASE: usize = 2;
+        self.ins
+            .iter()
+            .rev()
+            .enumerate()
+            .map(|(n, &r)| BASE.pow(n as u32) * matches!(r, b'R' | b'B') as usize)
+            .sum()
     }
 }
 
-impl<'a> Day<'a> for Day05<'a> {
+impl<'a> Day<'a> for Day05 {
     fn setup(&mut self, input: &'a str) -> Result<()> {
-        self.seats = input
+        self.seat_ids = input
             .lines()
             .map(|instructions| {
                 let b = instructions.as_bytes().try_into()?;
-                Ok(Seat::new(b))
+                Ok(Seat::new(b).get_seat_id())
             })
             .collect::<Result<Vec<_>>>()?;
+        self.seat_ids.sort();
         Ok(())
     }
 
     fn part1(&self) -> Result<String> {
-        let max = self
-            .seats
-            .iter()
-            .map(|s| s.get_seat_id())
-            .max()
-            .ok_or("Couldn't find max")?;
-        Ok(max.to_string())
+        match self.seat_ids.as_slice() {
+            [.., max] => Ok(max.to_string()),
+            _ => Err("No seats".into()),
+        }
     }
 
     fn part2(&self) -> Result<String> {
-        let mut seat_ids: Vec<_> = self.seats.iter().map(|s| s.get_seat_id()).collect();
-        seat_ids.sort();
-        let min = seat_ids.get(0).ok_or("No seat IDs")?;
-        let max = seat_ids.iter().last().ok_or("No seat IDs")?;
-
-        let seat = seat_ids
-            .iter()
-            .zip(*min..*max)
-            .filter_map(|(&s, v)| if s != v { Some(s - 1) } else { None })
-            .next()
-            .ok_or("No seat found")?;
-
-        Ok(seat.to_string())
+        match self.seat_ids.as_slice() {
+            [min, .., max] => Ok(self
+                .seat_ids
+                .iter()
+                .zip(*min..*max)
+                .filter_map(|(&s, v)| if s != v { Some(s - 1) } else { None })
+                .next()
+                .ok_or("No seat found")?
+                .to_string()),
+            _ => Err("No seats".into()),
+        }
     }
 }
