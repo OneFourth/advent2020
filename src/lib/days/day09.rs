0c1ae2ca607a2629905bbcf9f7476fea5e84237e
Jacques Schutte <jqschutte@gmail.com>
2020-12-09 07:22:52 +0200

    Day 9 initial attempt

--- src/lib/days/day09.rs
+++ src/lib/days/day09.rs
@@ -1,19 +1,39 @@
 use crate::lib::advent::Result;
+use crate::lib::utils::parse_lines;
 use crate::Day;
 
 #[derive(Default)]
-pub struct Day09;
+pub struct Day09 {
+    numbers: Vec<isize>,
+}
 
 impl<'a> Day<'a> for Day09 {
-    fn setup(&mut self, _input: &'a str) -> Result<()> {
+    fn setup(&mut self, input: &'a str) -> Result<()> {
+        self.numbers = parse_lines(input)?;
         Ok(())
     }
 
     fn part1(&self) -> Result<String> {
-        Ok("".to_string())
+        let v = self
+            .numbers
+            .windows(25)
+            .zip(self.numbers.iter().skip(25))
+            .find(|(w, n)| !w.iter().any(|a| w.contains(&(*n - *a))))
+            .ok_or("Could not find number")?;
+        Ok(v.1.to_string())
     }
 
     fn part2(&self) -> Result<String> {
-        Ok("".to_string())
+        let target: isize = self.part1()?.parse()?;
+        let mut n = 2;
+        let mut result = 0;
+        while n < self.numbers.len() {
+            if let Some(r) = self.numbers.windows(n).find(|ns| ns.iter().sum::<isize>() == target) {
+                result = r.iter().min().unwrap() + r.iter().max().unwrap();
+            }
+
+            n += 1;
+        }
+        Ok(result.to_string())
     }
 }
